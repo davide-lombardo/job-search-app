f2be54b874b5ea3cb532a0465c964d76fc2050df
davide-lombardo <davide.lombardo.92@gmail.com>
2022-05-15 20:42:04 +0200

    firebase config

--- src/app/services/http.service.spec.ts
+++ src/app/services/http.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 import { HttpService } from './http.service';
 
 describe('HttpService', () => {
   let service: HttpService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HttpService],
+    });
     service = TestBed.inject(HttpService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('#job should return an object', async () => {
+    let response: any;
+    const jobId = 1;
+    const expectedType = 'object';
+    const expectedUrl = 'https://www.themuse.com/api/public/jobs/' + String(jobId);
+
+    service.getJobDetails(jobId).subscribe((data) => {
+      response = data;
+    });
+
+    const request = httpTestingController.expectOne(expectedUrl);
+    expect(request.request.method).toBe('GET');
+
+    request.flush({});
+
+    expect(typeof response).toEqual(expectedType);
+  });
+
 });
